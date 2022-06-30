bdb2b259dbe8195f9abe6b603793c6bc04513048
new92 <94779840+new92@users.noreply.github.com>
2022-06-30 15:21:46 +0300

    Update Install Conventer.py

--- Trojan/Install Conventer.py
+++ Trojan/Install Conventer.py
@@ -1,3 +1,12 @@
-import os
+from os import system
+import time
 
-os.system("pip install auto-py-to-exe")
+osD=input("Please enter the type of your Operating System [Linux/Windows]: ")
+while osD != "Linux" and osD != "Windows" or osD == " " or osD == "":
+  print("Invalid Operating System !")
+  time.sleep(2)
+  osD=input("Please enter the type of your Operating System [Linux/Windows]: ")
+if osD == "Linux":
+  system("sudo pip3 install auto-py-to-exe")
+else:
+  system("pip install auto-py-to-exe")
