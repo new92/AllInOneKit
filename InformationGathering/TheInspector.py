0646ce6f71429729f1b72bf5bcf1e272d6c862f5
new92 <94779840+new92@users.noreply.github.com>
2022-07-12 09:35:22 +0300

    Update TheInspector.py

--- InformationGathering/TheInspector.py
+++ InformationGathering/TheInspector.py
@@ -1,6 +1,6 @@
-﻿"""
+"""
 Author: @new92
-Program for information gathering for Social Media accounts 
+TheInspector: Program for information gathering for Social Media accounts 
 Made for educatinoal purposes
 The author has no responsibility for any illegal activity/activities carried out using this tool
 """
@@ -37,12 +37,14 @@ try:
     from datetime import datetime
     from os import system
 except ImportError as imp:
-    print("Error While Importing Modules !")
+    print("[!] Error While Importing Modules !")
     time.sleep(2)
-    print("Ignoring Error...")
+    print("[+] Ignoring Error...")
     time.sleep(2)
-    system("sudo pip3 install -r requirementsI.txt")
-    pass
+    if platform.system == "Windows":
+        system("pip3 install -r requirementsI.txt")
+    else:
+        system("sudo pip3 install -r requirementsI.txt")
 #End of imports
 
 
@@ -50,23 +52,31 @@ except ImportError as imp:
 snif = pyfiglet.figlet_format("SNIFFER")
 print(snif)
 
-username=input("Please enter the username: ")
+username=input("[+] Please enter the username: ")
+while len(username) == 0:
+    print("[!] Invalid Username !")
+    time.sleep(1)
+    username=input("[+] Please enter again the username: ")
 time.sleep(1)
-socialplatform=input("Please enter the social media platform: ")
+socialplatform=input("[+] Please enter the social media platform: ")
+while len(socialplatform) == 0:
+    print("[!] Invalid Social Media Platform !")
+    time.sleep(1)
+    socialplatform=input("[+] Please enter again the social media platform: ")
 socialplatform=socialplatform.lower()
 socialplatform=socialplatform.strip()
 username=username.lower()
 username=username.strip()
 #Information Gathering
 
-if socialplatform == "instagram":
+if socialplatform == "instagram" or "Instagram":
     loader = instaloader.Instaloader()
     client=instagrapi.Client()
-    IsPrivate=input("Is the account private [Y/N] ? ")
+    IsPrivate=input("[+] Is the account private [Y/N] ? ")
     while IsPrivate != "Y" and IsPrivate != "y" and IsPrivate != "N" and IsPrivate != "n":
-        print("Invalid Input !")
+        print("[!] Invalid Input !")
         time.sleep(2)
-        IsPrivate=input("Is the social media platform Instagram [Y/N] ? ")
+        IsPrivate=input("Is the account private [Y/N] ? ")
     if IsPrivate == "Y" or IsPrivate == "y":
         try:
             profileinfo = loader.download_profile(username,True)
@@ -79,61 +89,86 @@ if socialplatform == "instagram":
             pass
         posts = loader.anonymous_copy()
 else:
-    pass
-userinfo = requests.get("https://www."+str(socialplatform)+".com/"+str(username))
-UsefulInfo=userinfo.headers
-OtherInfo=userinfo.content
+    userinfo = requests.get("https://www."+str(socialplatform)+".com/"+str(username))
+    UsefulInfo=userinfo.headers
+    OtherInfo=userinfo.content
 if userinfo.status_code == requests.codes.ok: 
-    print("User and information found !")
+    print("[!] User and information found !")
 else: 
-    print("User not found !")
-    time.sleep(2)
-    print("Try checking the username and the platform")
+    print("[!] User not found !")
+    time.sleep(1)
+    print("[+] Try checking the username and the platform")
 Information = UsefulInfo, OtherInfo
 #End of Information Gathering
 
-#Displaying Information which have been gathered
-time.sleep(2)
-print("Account found | ✓")
-time.sleep(3)
-print("Checking internet connection | ✓")
-time.sleep(5)
-print("Checking account's security | ✓")
-time.sleep(5)
-print("Initiating attack | ✓")
-time.sleep(5)
-print("Bypassing first security stage | ✓")
-time.sleep(5)
-print("Bypassing second security stage | ✓")
-time.sleep(5)
-print("Bypassing third security stage | ✓")
-time.sleep(5)
-print("Attack successful | ✓")
-time.sleep(5)
-print("Initiating information gathering | ✓")
-time.sleep(7)
-print("Password found | ✓")
-time.sleep(5)
-print("IP found | ✓")
-time.sleep(5)
-print("Device's information found | ✓")
-time.sleep(5)
-print("Gathering other information | ✓")
-time.sleep(5)
-print("Information gathered | ✓")
-time.sleep(5)
-print("This is the profile Sniffer formed with the information: ")
+#Displaying Gathered Information
+
+for i in tqdm(range(1000000)):
+    pass
+time.sleep(1)
+print("[+] Account found | ✓")
+for i in tqdm(range(1000000)):
+    pass
+time.sleep(1)
+print("[+] Checking internet connection | ✓")
+for i in tqdm(range(1000000)):
+    pass
+time.sleep(1)
+print("[+] Checking account's security | ✓")
+for i in tqdm(range(1000000)):
+    pass
+time.sleep(1)
+print("[+] Initiating attack | ✓")
+for i in tqdm(range(1000000)):
+    pass
+time.sleep(1)
+print("[+] Bypassing security | ✓")
+for i in tqdm(range(1000000)):
+    pass
+time.sleep(1)
+print("[+] Attack successful | ✓")
+for i in tqdm(range(1000000)):
+    pass
+time.sleep(1)
+print("[+] Initiating information gathering | ✓")
+for i in tqdm(range(10000000)):
+    pass
+time.sleep(1)
+print("[+] Password found | ✕")
+for i in tqdm(range(1000000)):
+    pass
+time.sleep(1)
+print("[+] IP found | ✕")
+for i in tqdm(range(1000000)):
+    pass
+time.sleep(1)
+print("[+] Device's information found | ✕")
+for i in tqdm(range(1000000)):
+    pass
+time.sleep(1)
+print("[+] Gathering other information | ✓")
+for i in tqdm(range(1000000)):
+    pass
+time.sleep(1)
+print("[+] Information gathered | ✓")
+for i in tqdm(range(1000000)):
+    pass
+time.sleep(1)
+print("[+] This is the profile Sniffer formed with the information: ")
 time.sleep(4)
 print("|----------------PROFILE--------------|                 ")
 print("                                                        ")
 time.sleep(2)
-print("      Platform: "+str(Socialplatform)+"                 ")
+print("    [+]  Platform: "+str(Socialplatform)+"                 ")
 time.sleep(2)
-print("      Username: "+str(username)+"                       ")
+print("    [+]  Username: "+str(username)+"                       ")
 time.sleep(2)
-print("      A file with the name: "+str(username)+" has been created, containing all the information for the user !")
-time.sleep(2)
-print("      Information: "+str(Information)+"                 ")
+if socialplatform == "instagram" or "Instagram":
+    print(" [+] A file with the name: "+str(username)+" has been created, containing all the information for the user !")
+    time.sleep(2)
+else:
+    pass
+print("     [+] Information: "+str(Information)+"                 ")
 time.sleep(2)
 print("                                                        ")
 print("|---------------------------------------|               ")
