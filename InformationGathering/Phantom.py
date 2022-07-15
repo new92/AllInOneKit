cd5ccd8ee60ef97f2b7555e83060df02880d8612
new92 <94779840+new92@users.noreply.github.com>
2022-07-16 00:22:00 +0300

    Update Phantom.py

--- InformationGathering/Phantom.py
+++ InformationGathering/Phantom.py
@@ -1,8 +1,8 @@
-﻿"""
+"""
 Author: @new92
 Phantom: Tool for Website Information Gathering
 Made for educational purposes 
-The author has no responsibility for any illegal activity/activities carried out using this program
+The author has no responsibility for any illegal activity/activities carried out with this tool
 """
 
 
@@ -16,25 +16,27 @@ try:
     import time
     import requests 
     import sniffer
-    import crypto
-    import cryptography
-    import Cryptodome
     import pyfiglet
-    import whois
-    import tqdm
-    import re
+    import scrapy
     import nmap
+    import whois
+    import ipwhois
+    import ipapi
+    import re
+    import tqdm
     import argparse
+    import builtwith
+    import platform
     from os import system
-except ImportError as i:
+except ImportError as imp:
     print("[!] WARNING: Not all modules used in this program have been installed !")
     time.sleep(2)
     print("[+] Ignoring Warning...")
     time.sleep(2)
     if platform.system == "Windows":
-        system("pip3 install -r requirementsP.txt")
+        system("pip3 install -r requirements.txt")
     else:
-        system("sudo pip3 install -r requirementsP.txt")
+        system("sudo pip3 install -r requirements.txt")
 #End of imports
 
 #Logo
@@ -42,75 +44,117 @@ phantom=pyfiglet.figlet_format("PHANTOM")
 print(phantom)
 #
 
-#Information Gathering
-IPport=socket.IPPORT_RESERVED
-website=input("[+] Please enter the name of the website: ")
-while website == " " or website == "" or len(website) = 0 or "http" not in website or "https" not in website:
-    print("[!] Invalid Website !")
+class Information:
+    def __init__(self,wbsite):
+        self.wbsite = wbsite
+    def IsRegist(self):
+        try:
+            who = whois.whois(self.wbsite)
+        except Exception:
+            return False
+        else:
+            return bool(who.domain_name)
+    def Content(self):
+        contents = builtwith.builtwith(self.wbsite)
+        return contents
+    def FullHName(self):
+        FName=socket.getfqdn(wip)
+        return FName
+    def Location(self):
+        loc = ipapi.location(ip = wip)
+        return loc
+
+def OpenPorts(ips):
+    op = system("nmap --open "+str(ips))
+    return op
+print("[+] Github: @new92")
+print("\n")
+print("[01] Information Gathering")
+print("[02] Exit")
+print("\n")
+option=input("[::] Choose an option: ")
+
+while option != "01" and option != "02" and option != "1" and option != "2":
     time.sleep(1)
-    website=input("[+] Please enter again the name of the website: ")
-website=website.lower()
-website=website.strip()
-Info=requests.get("https://www."+str(website)+".com")
-time.sleep(1)
-if Info.status_code == requests.codes.ok:
-    print("[!] Information Gathering Successfull !")
-else:
-    print("[!] Website not found !!")
+    print("[!] Invalid option !")
     time.sleep(1)
-    print("[+] Please check the name and try again !")
-MoreInfo=Info.headers
-UsefulInfo=Info.content
-try: 
-    IPwebsite=socket.gethostbyname("www."+str(website)+".com")
-except socket.herror() as s:
-    print("[!] Error while tracing the IP of the website !")
+    option=input("[::] Please enter again: ")
+if option == "01" or option == "1":
+    time.sleep(1)
+
+    #Information Gathering
+    
+    IPport=socket.IPPORT_RESERVED
+    website1=input("[+] Please enter the name of the website: ")
+    while website1 == " " or website1 == "" or len(website1) == 0:
+        print("[!] Invalid Website !")
+        time.sleep(1)
+        website1=input("[+] Please enter again the name of the website: ")
+    website=website1.lower()
+    website=website.strip()
+    hname = str(website)+".com"
+    Info=requests.get("https://www."+str(website)+".com")
+    time.sleep(1)
+    if Info.status_code == requests.codes.ok:
+        print("[!] Information Gathering Successfull !")
+    else:
+        print("[!] Website not found !")
+        time.sleep(1)
+        print("[+] Please check the name and try again !")
+        exit(0)
+    UsefulInfo=Info.content
+    website = "https://www."+str(website)+".com"
+    info = Information(website)
+    wip = socket.gethostbyname(hname)
+
+    # Main program
+    for i in tqdm(range(10000000)):
+        pass
+    time.sleep(1)
+    print("[+] Website found | ✓")
+    for i in tqdm(range(10000000)):
+        pass
+    time.sleep(1)
+    print("[+] Checking internet connection | ✓")
+    for i in tqdm(range(10000000)):
+        pass
+    time.sleep(1)
+    print("[+] Checking website's security | ✓")
+    for i in tqdm(range(10000000)):
+        pass
+    time.sleep(1)
+    print("[+] Bypassing security | ✓")
+    for i in tqdm(range(10000000)):
+        pass
+    time.sleep(1)
+    print("[+] Initiating information gathering | ✓")
+    for i in tqdm(range(10000000)):
+        pass
+    time.sleep(1)
+    print("[+] Information gathered | ✓")
     time.sleep(2)
-    print(s)
-FullHostName=socket.getfqdn(IPwebsite)
-
-#End of Information Gathering
-
-#Displaying Information / Main program
-for i in tqdm(range(1000000)):
-    pass
-time.sleep(1)
-print("[+] Website found | ✓")
-for i in tqdm(range(1000000)):
-    pass
-time.sleep(1)
-print("[+] Checking internet connection | ✓")
-for i in tqdm(range(1000000)):
-    pass
-time.sleep(1)
-print("[+] Checking website's security | ✓")
-for i in tqdm(range(1000000)):
-    pass
-time.sleep(1)
-print("[+] Bypassing security | ✓")
-for i in tqdm(range(1000000)):
-    pass
-time.sleep(1)
-print("[+] Initiating information gathering | ✓")
-for i in tqdm(range(1000000)):
-    pass
-time.sleep(1)
-print("[+] Information gathered | ✓")
-for i in tqdm(range(1000000)):
-    pass
-time.sleep(1)
-print("[+] This is the profile of the website Phantom formed with the information:")
-time.sleep(2)
-print("|----------------PROFILE--------------------|             ")
-print("                                                          ")
-print("  [+] Full Host Name: "+str(FullHostName)+"               ")
-time.sleep(2)
-print("  [+] Website IP: "+str(IPwebsite)+"                      ")
-time.sleep(2)
-print("  [+] Other Informations: "+str(UsefulInfo)+"             ")
-time.sleep(2)
-print("  [+] Useful Informations: "+str(MoreInfo)+"              ")
-time.sleep(2)
-print("                                                          ")
-print("|-------------------------------------------|             ")
-#End of the program
+    print("[+] This is the profile of "+str(website1)+" Phantom formed with the information:")
+    time.sleep(2)
+    print("|----------------"+str(website1.upper())+"--------------------|")
+    print("                                                          ")
+    print("   [+] Full Host Name: "+str(info.FullHName())+"         ")
+    time.sleep(2)
+    print("   [+] Website IP: "+str(wip)+"                    ")
+    time.sleep(2)
+    print("   [+] Has Registered Domain Name: "+str(info.IsRegist())+" ")
+    time.sleep(2)
+    print("   [+] Has Been Built With: "+str(info.Content())+"      ")
+    time.sleep(2)
+    print("   [+] Website Link: "+str(website)+"                    ")
+    time.sleep(2)
+    print("   [+] Website Location: "+str(info.Location())+"        ")
+    time.sleep(2)
+    print("   [+] Website Open Ports: "+str(OpenPorts(wip))+"              ")
+    time.sleep(2)
+    print("   [+] Other Information: "+str(UsefulInfo)+"                   ")
+    time.sleep(2)
+    print("                                                                ")  
+    print("|-------------------------------------------|                   ")
+else:
+    print("[+] Exiting...")
+    exit(0)
