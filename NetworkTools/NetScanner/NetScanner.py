5f26b3683ad1a424d5fc49c855520777f9296e54
new92 <94779840+new92@users.noreply.github.com>
2022-07-27 00:43:39 +0300

    Update NetScanner.py

--- NetworkTools/NetScanner/NetScanner.py
+++ NetworkTools/NetScanner/NetScanner.py
@@ -1,61 +1,67 @@
 """
 Author: @new92
-
 NetScanner: Program for Network Scanning !
 """
 
 #Imports
 
 try:
+    import time
+    import platform
+    from os import system
     import socket
     import http
     import websockets
-    import time
     import sys
     import os
     import nmap
-    import pyfiglet
+    import sniffer
     import tqdm
-    import platform
+    import art
+    from art import tprint
     from socket import *
+    from tqdm import tqdm
     from scapy.all import ARP, Ether, srp
 except ImportError as imp:
     print("[!] WARNING: Not all modules used in this program have been installed !")
     time.sleep(2)
     print("[+] Ignoring Warning...")
-    time.sleep(2)
+    time.sleep(1)
     if platform.system == "Windows":
         system("pip3 install -r requirements.txt")
     else:
         system("sudo pip3 install -r requirements.txt")
-#End of Imports
+
 
 #Main Program
-netscanner=pyfiglet.figlet_format("NetScanner")
-print(netscanner)
+tprint("NETSCANNER",font="tarty1")
 
-time.sleep(1)
-print("[+] Network Scanner")
 print("\n")
-time.sleep(2)
+print("[+] Program for Network Scanning")
+print("\n")
 print("[+] Github: @new92")
 print("\n")
 print("[01] Scan Network for Vulnerabilities")
 print("[02] Display Active Devices in the Network")
 print("[0] Exit")
-option=input("[::] Choose an option: ")
-while option != "01" and option != "02" and option != "0" and option != "1" and option != "2":
+option=int(input("[::] Choose an option: "))
+while option < 0 or option > 2 or option == None:
     print("[!] Invalid option !")
     time.sleep(1)
     option=input("[::] Please enter again: ")
-if option == "01" or option == "1":
+if option == 1:
     time.sleep(1)
-    ip=input("[+] Please enter the ip address of the router: ")
-    while "." not in ip:
+    ip=input("[::] Please enter the ip address of the router: ")
+    while "." not in ip or ip == None:
         print("[!] Invalid IP Address !")
         time.sleep(2)
-        ip=input("[+] Please enter again the IP of the router: ")
+        ip=input("[::] Please enter again the IP of the router: ")
 
+    lport = input("[::] Please enter the last port to scan: ")
+    while lport == None:
+        print("[!] Invalid Parameter !")
+        time.sleep(1)
+        lport = input("[::] Please enter the last port to scan: ")
     print("[+] Please wait while the program is scanning the network...")
     for b in tqdm(range(1000000)):
         pass
@@ -64,12 +70,12 @@ if option == "01" or option == "1":
         host = gethostbyname(ip)
 
         connection = sock.connect_ex((host, i))
-        if connection == 0:
+        if connection == True:
             print("Port "+str(i)+": OPEN")
         sock.close()
 elif option == "02" or option == "2":
     time.sleep(1)
-    rout_ip=input("[+] Please enter the IP address of the router: ")
+    rout_ip=input("[::] Please enter the IP address of the router: ")
     arp = ARP(pdst=rout_ip)
     ether = Ether(dst="ff:ff:ff:ff:ff:ff")
     packet = ether/arp
@@ -86,5 +92,4 @@ elif option == "02" or option == "2":
     print("<END>")
 else:
     print("[+] Exiting...")
-    quit(0)
-#End of the Program
+    exit(0)
