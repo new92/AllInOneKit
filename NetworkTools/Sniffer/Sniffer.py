31c0e0bea5ea2196af7d00b285ed155c6c833d30
new92 <94779840+new92@users.noreply.github.com>
2022-07-27 00:53:09 +0300

    Update Sniffer.py

--- NetworkTools/Sniffer/Sniffer.py
+++ NetworkTools/Sniffer/Sniffer.py
@@ -1,23 +1,27 @@
 """
 Author: @new92
 Sniffer: Program for Sniffing Packets in the Network
+The Author has no responsibility for the use of this program
 """
+
 #Imports
+
 try:
+    import time
+    import platform
+    from os import system
     import sniffer
     import nmap
     import os
     import sys
-    import time
-    import pyfiglet
-    import platform
     import scapy.all as scapy
     import argparse
+    import art
+    from art import tprint
     from scapy.layers import http
-    from os import system
 except ImportError as imp:
     print("[!] WARNING: Not all modules used in this program have been installed !")
-    time.sleep(1)
+    time.sleep(2)
     print("[+] Ignoring Warning...")
     time.sleep(1)
     if platform.system == "Windows":
@@ -26,15 +30,19 @@ except ImportError as imp:
         system("sudo pip3 install -r requirements.txt")
 
 #Logo
-snif = pyfiglet.figlet_format("SNIFFER")
-print(snif)
+
+tprint("SNIFFER",font="tarty1")
 
 #Defs
+
 def Intface():
-    parse = argparse.ArgumentParser()
-    parse.add_argument("-i", "--interface", dest="interface", help="Interface to sniff packets on")
-    args = parse.parse_args()
-    return args.interface
+    time.sleep(2)
+    intface = input("[+] Please enter the interface: ")
+    while intface == None:
+        print("[!] You must enter an Interface in order for the program to work !")
+        time.sleep(2)
+        intface = input("[+] Please enter again the interface: ")
+    return intface
 
 def Spoof(iface):
     scapy.sniff(iface=iface, store=False, prn=SniffPack)
@@ -47,12 +55,25 @@ def SniffPack(packet):
             keys = ["username","password","pass","email"]
             for key in keys():
                 if key in load:
-                    print("[+] Captured Possible Password/Username/Email --> "+str(load))
-                    break
+                    print("[!] Captured Possible Password/Username/Email --> "+str(load))
 #Main Program
+
+print("\n")
+print("[+] Program for Sniffing and Capturing Packets in the network !")
+print("\n")
+print("[+] Github: @new92")
+print("\n")
+print("[1] Initiate Sniffer")
+print("[2] Exit")
+option = int(input("[::] Please enter the choice: "))
+while option <= 0 or option > 2 or option == None:
+    print("[!] Invalid Choice !")
+    time.sleep(1)
+    option=int(input("[::] Please enter again the choice: "))
+
+iface = Intface()
 time.sleep(1)
 print("[+] Sniffer Started...")
 time.sleep(2)
 print("[+] Please wait while the program is gathering packets...")
-iface = Intface()
 Spoof(iface)
